068bf09d22c1dd76889cf1592a516c57eb1372e8
morumoru0110 <morumoru0110@yahoo.co.jp>
2020-07-23 21:42:50 +0900

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name_kanji,:first_name_kanji,:last_name_kana,:first_name_kana,:birthday,:phone_number])
+  end
+
 end
---
+++ db/migrate/20200723073340_add_column_to_user.rb
@@ -0,0 +1,11 @@
+class AddColumnToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :nickname, :string, null: false
+    add_column :users, :last_name_kanji, :string, null: false
+    add_column :users, :first_name_kanji, :string, null: false
+    add_column :users, :last_name_kana, :string, null: false
+    add_column :users, :first_name_kana, :string, null: false
+    add_column :users, :birthday, :integer, null: false
+    add_column :users, :phone_number, :integer, null: false
+  end
+end
