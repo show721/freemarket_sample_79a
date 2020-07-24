35c67b03985562400e25f14e768da0e096fbdce4
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-07-25 08:27:20 +0900

    add validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_many :images
+  
   validates :name, :description, :category, :brand, :price, :condition, :shipping_charge, :shipping_area, :shipping_day, presence: true
+  validates :price, length: { minimum: 3 }
   accepts_nested_attributes_for :images, allow_destroy: true
 end
