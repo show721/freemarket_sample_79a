441afce81fac93f998da0b11949b8a4699603e47
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-07-24 21:11:46 +0900

    add new.html.haml

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,11 +9,12 @@ class ProductsController < ApplicationController
   end
 
   def create
+    binding.pry
     @product = Product.new(product_params)
     if @product.save
       redirect_to root_path
     else
-      render :new
+      redirect_to products_path
     end
   end
 
@@ -29,6 +30,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, images_attributes: [:src])
+    params.require(:product).permit(:name, :description , :category , :brand ,:price ,:condition , :shipping_charge ,:shipping_area ,:shipping_day ,images_attributes: [:image])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :images
+  validates :name, :description, :category, :brand, :price, :condition, :shipping_charge, :shipping_area, :shipping_day, presence: true
   accepts_nested_attributes_for :images, allow_destroy: true
 end
