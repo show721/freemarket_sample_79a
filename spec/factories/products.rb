07bbb64b9497837c62d017005afee67257bbdf70
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-07-28 14:05:41 +0900

    add RSpec test

---
+++ spec/factories/products.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+
+  factory :product do
+    name              {"ジャケット"}
+    description       {"新品です"}
+    category          {111}
+    brand             {111}
+    price             {300}
+    condition         {"新品"}
+    shipping_charge   {1}
+    shipping_area     {"東京"}
+    shipping_day      {1}
+  end
+
+end
