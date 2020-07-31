d76c7fe3a811319e748f090ac44d7d72188c5198
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-08-01 00:06:30 +0900

    add valid product

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,5 +7,6 @@ class Product < ApplicationRecord
   validates :name, length: { maximum: 40 }
   validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :shipping_charge, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 2}
+  validates :shipping_day, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 7}
   accepts_nested_attributes_for :images, allow_destroy: true
 end
