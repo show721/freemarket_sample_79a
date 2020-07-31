23553b18c3ffea3638cb29ac7987d780d42c5e83
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-07-31 20:53:18 +0900

    moved jsfaile

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,13 +14,13 @@ class ProductsController < ApplicationController
     end
   end
 
-   def get_category_children
-    @category_children = Category.find_by(category: "#{params[:product_category]}", ancestry: nil).children
-   end
+  def get_category_children
+   @category_children = Category.find_by(category: "#{params[:product_category]}", ancestry: nil).children
+  end
 
-   def get_category_grandchildren
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
-   end
+  def get_category_grandchildren
+   @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
 
   def create
     @product = Product.new(product_params)
@@ -44,7 +44,7 @@ class ProductsController < ApplicationController
     if @product.update_attributes(product_params)
       redirect_to root_path
     else
-      render :edit
+      render :new
     end 
   end
 
@@ -72,7 +72,9 @@ class ProductsController < ApplicationController
                                     images_attributes: [:image, :_destroy, :id])
   end
 
-  
+  def set_category
+    
+  end
 
 
 end
