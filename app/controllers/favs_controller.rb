af5f6d80ad8b54df6180b7d5031c889b57918361
show721 <y.s.s.t721@gmail.com>
2020-08-09 00:26:23 +0900

    非同期通信完了

--- app/controllers/favs_controller.rb
+++ app/controllers/favs_controller.rb
@@ -3,6 +3,6 @@ class FavsController < ApplicationController
   def create
     product_id = Product.find(params[:id])
     Fav.create(user_id:current_user.id, product_id:product_id.id)
-    redirect_to product_path(product_id.id)
   end
+  
 end
