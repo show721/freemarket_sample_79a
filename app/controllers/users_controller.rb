844c774782550dbb5621ad6a2f730ac14ae685fb
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-08-09 14:00:08 +0900

    fixed user mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @user = User.find_by(id: current_user.id)
+    @buyer = Product.where(buyer_id: current_user.id).order("created_at DESC").limit(5)
   end
 
   def new
