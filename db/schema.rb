3cbeb3b5955ff447f2fb790171cc666c3506c772
s79ns <67348810+s79ns@users.noreply.github.com>
2020-08-10 12:25:49 +0900

    出品者が出品した物に対し、出品者のみがコメントできるバグを修正

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_042341) do
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", limit: 255, null: false
+    t.string "image", null: false
     t.bigint "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -77,14 +77,14 @@ ActiveRecord::Schema.define(version: 2020_08_06_042341) do
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", limit: 255, null: false
+    t.string "name", null: false
     t.text "description", null: false
-    t.integer "category_id", null: false
-    t.string "brand", limit: 255
+    t.string "category_id", null: false
+    t.string "brand"
     t.integer "price", null: false
-    t.string "condition", limit: 255, null: false
+    t.string "condition", null: false
     t.integer "shipping_charge", null: false
-    t.string "shipping_area", limit: 255, null: false
+    t.string "shipping_area", null: false
     t.integer "shipping_day", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
