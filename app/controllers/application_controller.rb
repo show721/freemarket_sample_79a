5266d86de304d54c918e08b6b00c3af9fefa00f4
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-08-10 14:49:56 +0900

    add basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   # protect_from_forgery
 
@@ -8,5 +9,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name_kanji,:first_name_kanji,:last_name_kana,:first_name_kana,:birthday])
   end
 
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == 'abc1'
+    end
+  end
 end
 
